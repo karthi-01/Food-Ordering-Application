318f047d55d5ad0ccd066e063ad16d8b5702b24e
unknown <durga.late@gmail.com>
2023-06-04 23:37:37 +0530

    changed address UI

--- food-ordering-app/src/components/AddressForm.jsx
+++ food-ordering-app/src/components/AddressForm.jsx
@@ -3,44 +3,7 @@ import Button from "./elements/Button";
 import { ReactComponent as ArrowRightSvg } from "../assets/icons/arrow-right-long-svgrepo-com.svg";
 import { useDispatch } from "react-redux";
 import { setAddress } from "../stores/userInfo/addressSlice";
-import React, { useState } from 'react';
-import Select from 'react-select';
-
-const cityOptions = [
-    { value: 'New York', label: 'New York' },
-    { value: 'London', label: 'London' },
-    { value: 'Paris', label: 'Paris' },
-    { value: 'Tokyo', label: 'Tokyo' },
-    // Add more city options as needed
-  ];
-
-  const Autocomplete = () => {
-    const [selectedCity, setSelectedCity] = useState(null);
-  
-    const handleCityChange = (selectedOption) => {
-      setSelectedCity(selectedOption);
-    };
-  return (
-      <div>
-        <h1>City </h1>
-        <Select
-          value={selectedCity}
-          onChange={handleCityChange}
-          options={cityOptions}
-          placeholder="Type a city name..."
-        />
-        {selectedCity && (
-          <div>
-            <h2>Selected City:</h2>
-            <p>{selectedCity.label}</p>
-          </div>
-        )}
-      </div>
-    );
-  };
-  
-//export default Autocomplete;
-
+import { useState, useEffect } from 'react';
 export const AddressForm = ({ onTabSwitch }) => {
     const { register, handleSubmit, formState: { errors }} = useForm();
     const dispatch = useDispatch();
@@ -50,9 +13,20 @@ export const AddressForm = ({ onTabSwitch }) => {
         onTabSwitch('Payment');
     }
 
+    const [selectedState, setSelectedState] = useState('');
+  const [selectedCity, setSelectedCity] = useState('');
+
+  const handleStateChange = (event) => {
+    setSelectedState(event.target.value);
+  };
+
+  const handleCityChange = (event) => {
+    setSelectedCity(event.target.value);
+  };
+
     return (
         <form className="md:w-2/3 md:mx-auto px-3 pt-1" onSubmit={handleSubmit(onSubmit)}>
-            <h3 className="pt-4 text-2xl md:text-center">Address for the delivery</h3>
+            <h3 className="pt-4 text-2xl md:text-center block mb-2  font-bold ">Address for the delivery</h3>
             <div className="mb-4">
                 <label className="block mb-2 text-sm font-bold text-gray-700" for="streetAddress">Street Address</label>
                 <input 
@@ -64,32 +38,65 @@ export const AddressForm = ({ onTabSwitch }) => {
                     />
                 {errors.address && <span className="text-red-500">This field is required</span>}
             </div>
-            <div className="mb-4 md:flex md:justify-between">
-                <div className="mb-4 md:mr-2 md:mb-0 flex-1">
-                    <label className="block mb-2 text-sm font-bold text-gray-700" for="city">City</label>
-                    <input 
-                    {...register('city')}
-                    className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border roundedn shadow appearance-none focus:outline-none focus:shadow-outline"
-                    id="city"
-                    type="text"
-                    placeholder="City"
-                    />
-                    <Autocomplete/>
-                    {errors.address && <span className="text-red-500">This field is required</span>}
-                </div>
-                <div className="mb-4 md:mr-2 md:mb-0 flex-1">
-                    <label className="block mb-2 text-sm font-bold text-gray-700" for="state">State</label>
-                    <input 
-                    {...register('state')}
-                    className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border roundedn shadow appearance-none focus:outline-none focus:shadow-outline"
-                    id="state"
-                    type="text"
-                    placeholder="state"
-                    />
-                    {errors.address && <span className="text-red-500">This field is required</span>}
-                </div>
-            </div>
-            <div className="mb-4 md:flex md:justify-between">
+            <div>
+    <div className="mb-4 md:flex md:justify-between">          
+      <div className="mb-4 md:mr-2 md:mb-0 flex-1">
+       <label className="block mb-2 text-sm font-bold text-gray-700" for="State">State</label>
+        <select value={selectedState} onChange={handleStateChange}>
+          <option value="">-- Select a state --</option>
+          <option value="Gujarat">Gujarat</option>
+          <option value="Maharashtra">Maharashtra</option>
+          <option value="Punjab">Punjab</option>
+          <option value="Tamil Nadu">Tamil Nadu</option>
+          <option value="Uttar Pradesh">Uttar Pradesh</option>
+        </select>
+      </div>
+      <div className="mb-4 md:mr-2 md:mb-0 flex-1">
+      <label className="block mb-2 text-sm font-bold text-gray-700" for="city">City</label>
+        <select value={selectedCity} onChange={handleCityChange}>
+          <option value="">-- Select a city --</option>
+          {selectedState === 'Gujarat' && (
+            <>
+              <option value="Ahmedabad">Ahmedabad</option>
+              <option value="Surat">Surat</option>
+              <option value="Vadodara">Vadodara</option>
+            </>
+          )}
+          {selectedState === 'Maharashtra' && (
+            <>
+              <option value="Mumbai">Mumbai</option>
+              <option value="Pune">Pune</option>
+              <option value="Thane">Thane</option>
+            </>
+          )}
+          {selectedState === 'Punjab' && (
+            <>
+              <option value="Chandigarh">Chandigarh</option>
+              <option value="Amritsar">Amritsar</option>
+              <option value="Ludhiana">Ludhiana</option>
+            </>
+          )}
+          {selectedState === 'Tamil Nadu' && (
+            <>
+              <option value="Chennai">Chennai</option>
+              <option value="Coimbatore">Coimbatore</option>
+              <option value="Madurai">Madurai</option>
+            </>
+          )}
+          {selectedState === 'Uttar Pradesh' && (
+            <>
+              <option value="Lucknow">Lucknow</option>
+              <option value="Varanasi">Varanasi</option>
+              <option value="Agra">Agra</option>
+              <option value="Kanpur">Kanpur</option>
+            </>
+          )}
+        </select>
+      </div>
+      </div>
+      {errors.address && <span className="text-red-500">This field is required</span>}
+    </div>
+    <div className="mb-4 md:flex md:justify-between">
                 <div className="mb-4 md:mr-2 md:mb-0 flex-1">
                     <label className="block mb-2 text-sm font-bold text-gray-700" for="postalCode">Pin Code</label>
                     <input 
