aad9cb370668d391ce64e3dd4ca289635ecaa51c
unknown <durga.late@gmail.com>
2023-06-04 18:01:37 +0530

    Removed Above from header

--- food-ordering-app/src/components/Header.jsx
+++ food-ordering-app/src/components/Header.jsx
@@ -62,13 +62,13 @@ export const Header = ({ cartCount, active, setActive }) => {
           <Link to="/" className={`"text-xl" ${path === "/" ? "active" : ""}`}>
             Home
           </Link>
-          <Link
+          {/*<Link
             to="#about"
             onClick={handleScroll}
             className={`"text-xl" ${path === "#about" ? "active" : ""}`}
           >
             About
-          </Link>
+  </Link>*/}
         </div>
         <div className="flex items-center justify-center space-x-4">
           <Link
@@ -110,36 +110,3 @@ export const Header = ({ cartCount, active, setActive }) => {
     </nav>
   );
 };
-
-    return (
-        <nav id="header" className="bg-black text-white">
-            <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
-                <div className="logo-wrapper pl-4 flex items-center">
-                    <Link to="/" className="toggleColor text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl">
-                        <img src={foody} alt="logo" className="w-40 h-40 object-cover"/>
-                    </Link>
-                </div>
-                <div className="nav-menu-wrapper flex items-center justify-between space-x-10">
-                    <Link to="/" className = {`"text-xl" ${path  === '/' ? "active" : ""}`}>Home</Link>
-                    {/* <Link to="#about" onClick={handleScroll} className = {`"text-xl" ${path  === '#about' ? "active" : ""}`}>About</Link> */}
-                </div>
-                <div className="flex items-center justify-center space-x-4">
-                    <Link to="/cart"  className = {`" mr-4 relative" ${path  === '/cart' ? "active" : "white"}`}>
-                        <i class="fa-solid fa-cart-shopping"></i>
-                        {cartCount > 0 ? <div className="rounded-full bg-yellow-400 text-white inline-flex justify-center items-center w-full absolute -top-1 -right-1">{cartCount}</div> : null}
-                    </Link>
-                    {
-                        isLoggedIn ?
-                        <Button onClick={handleLogout}>Log Out</Button> :
-                        (
-                            <>
-                             <Link to="/login" className = {`"text-xl" ${path  === '/login' ? "active" : ""}`}>Log In</Link>
-                             <Link to="/register" className = {`"text-xl" ${path  === '/register' ? "active" : ""}`}>Sign Up</Link>
-                            </>
-                        )
-                    }
-                </div>
-            </div>
-        </nav>
-    )
-}
--- food-ordering-app/src/stores/cart/cartSlice.js
+++ food-ordering-app/src/stores/cart/cartSlice.js
@@ -5,24 +5,6 @@ const initialState = {
 };
 
 export const cartSlice = createSlice({
-    name: 'cart',
-    initialState,
-    reducers: {
-        addToCart: (state, action) => {
-            return { products: [...state.products, {...action.payload, amount: 1}]}
-        },
-        clearCart: (state) => {
-            return { products: []}
-        },
-        incrementProductAmount: (state, action) => {
-            console.log(action);
-            return { products: state.products.map(product => product.id === action.payload._id ? {...product, amount: product.amount + 1} : product)}
-        },
-        decrementProductAmount: (state, action) => {
-            return { products: state.products.map(product => product.id === action.payload.id ? {...product, amount: product.amount - 1} : product)}
-        }
-    }
-})
   name: "cart",
   initialState,
   reducers: {
